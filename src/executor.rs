9ec949b8fd82fb80294cd0b17843a5f39f3505c7
Eric Crosson <eric.s.crosson@utexas.edu>
2024-12-03 13:11:43 -0600

    test: remove test that failed when building as a flake input

--- src/executor.rs
+++ src/executor.rs
@@ -470,36 +470,4 @@ mod tests {
             })
         ))
     }
-
-    // This test may become flaky if system load is high;
-    // feel free to delete or refactor this test if it becomes a problem
-    #[tokio::test]
-    async fn execute_retry_every() {
-        // Arrange
-        let number_of_times = 3;
-        let execution_interval = Duration::from_millis(10);
-        let min_expected_duration = execution_interval * (number_of_times - 1);
-        let max_expected_duration = execution_interval * number_of_times;
-        let limit = Limit::NumberOfTimes(number_of_times.into());
-
-        let decider = TestDecider(Decision::Unfinished(UnfinishedReason::Timeout));
-        let mut executor = Executor::new(DummyTask, decider, limit, Some(execution_interval));
-
-        // Act
-        let start = tokio::time::Instant::now();
-        let outcome = executor.execute().await;
-        let elapsed = start.elapsed();
-
-        // Assert
-        assert!(matches!(
-            outcome,
-            Err(ExecuteError {
-                kind: ExecuteErrorKind::Exhausted(ExhaustionReason::RetryTimesExceeded(
-                    UnfinishedReason::Timeout
-                ))
-            })
-        ));
-        assert!(min_expected_duration < elapsed);
-        assert!(elapsed < max_expected_duration);
-    }
 }
